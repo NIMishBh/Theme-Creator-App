db16a6f34d0dcc9b14c9698589c1bd1de34af579
nimishh <nimish864@gmail.com>
2021-05-06 15:08:14 +0530

    FIXED : Foreground Color now gets set

--- src/App.js
+++ src/App.js
@@ -11,28 +11,26 @@ function App() {
   console.log("Current Theme is : ", currentTheme);
 
   return (
-    <div className="App" style={{ background: currentTheme.bgcolor }}>
-      <div className="nav">
-        <h2>Theme Creator App</h2>
-        <ThemeSelector
-          title="Select a Theme"
-        />
+    <div>
+      <div className="App" style={{ background: currentTheme.bgcolor}}>
+        <div className="nav">
+          <h2>Theme Creator App</h2>
+          <p> Current Theme : {currentTheme.name}</p>
+          <ThemeSelector />
+        </div>
+        <div className="content">
+          <p style={{ color: currentTheme.fgcolor}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Dui aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <h1 style = {{color: currentTheme.fgcolor }}>Below You can Create a Theme and Then Apply the Theme From The Drop Down Above.</h1>
       </div>
-      <div className="content">
-        <p style={{ fontSize: "20px", color: currentTheme.fgColor }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Dui aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <h2 style={{ color: currentTheme.fgColor }}>Create A New Theme = </h2>
-      <div className="form-content" style={{ color: currentTheme.fgColor }}>
-        <ThemeCreationForm />
-      </div>
+      <ThemeCreationForm />
     </div>
   );
 }
--- src/components/ThemeCreationForm/index.js
+++ src/components/ThemeCreationForm/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { useDispatch } from "react-redux";
-import { AddTheme } from '../../redux/actions'
+import { AddTheme } from '../../redux/actions';
+import './style.css';
 
 
 function ThemeCreationForm() {
@@ -12,7 +13,7 @@ function ThemeCreationForm() {
   }
 
   return (
-    <div>
+    <div className="container">
       <Formik
         initialValues={{
           name: '',
@@ -33,34 +34,41 @@ function ThemeCreationForm() {
           handleBlur
         }) => (
           <form onSubmit={handleSubmit}>
-            <label>Theme Name: </label>
-            <input
-              type="text"
-              name="name"
-              value={values.name}
-              required
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <label>Background Color: </label>
-            <input
-              type="color"
-              name="bgcolor"
-              value={values.bgcolor}
-              required
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <label>Foreground Color: </label>
-            <input
-              type="color"
-              name="fgcolor"
-              value={values.fgcolor}
-              required
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <button type="submit" disabled={isSubmitting}>Submit</button>
+            <h2>Create A Theme : </h2>
+            <div>
+              <label>Theme Name : </label>
+              <input
+                type="text"
+                name="name"
+                value={values.name}
+                required
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+            <div>
+              <label>Background Color : </label>
+              <input
+                type="color"
+                name="bgcolor"
+                value={values.bgcolor}
+                required
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+            <div>
+              <label>Foreground Color : </label>
+              <input
+                type="color"
+                name="fgcolor"
+                value={values.fgcolor}
+                required
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+            <button className="button button-submit" type="submit" disabled={isSubmitting}>Add Theme</button>
           </form>
         )}
       </Formik>
